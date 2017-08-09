79edbdc1b36db0b67dc7c2a82698887f34868a50
fcu-d0449763 <d0449763@fcu.edu.tw>
2017-08-09 15:54:48 +0800

    change URL debug

--- game.js
+++ game.js
@@ -100,7 +100,7 @@ var CardGame = function(targetId)
 
   // template for card
   var card = document.createElement("div");
-  card.innerHTML = "<img src=images/back.png>";
+  card.innerHTML = "<img src=\"//fcu-d0449763.github.io/sitcon_camp/images/back.png\">";
 
   for(var i=0; i < 16; i++) {
     var newCard = card.cloneNode(true);
